1f8758f51952c187c013a2ed8f13747539a2513c
muhammad rizqi abdi ghifari <ghifaabdi@gmail.com>
2025-07-30 03:45:56 +0700

    Pesan commit yang sesuai

--- resources/views/components/fluxui-table.blade.php
+++ resources/views/components/fluxui-table.blade.php
@@ -1,5 +1,5 @@
 <div class="overflow-x-auto rounded shadow">
-    <table class="min-w-full bg-rose-100 text-gray-500 font-bold">
+    <table class="min-w-full bg-rose-100 text-gray-600 font-bold">
         {{ $slot }}
     </table>
 </div>
