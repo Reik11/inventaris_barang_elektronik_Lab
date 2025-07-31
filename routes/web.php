07cf81198cda4ad71bd1354cca0d5acaa63e9807
muhammad rizqi abdi ghifari <ghifaabdi@gmail.com>
2025-07-31 19:54:53 +0700

    perbaikan error code

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
 use App\Http\Controllers\BarangElektronikController;
-
+#routing
 Route::get('/', fn() => redirect()->route('barang.index'));
 Route::resource('barang', BarangElektronikController::class);
